287c01e99a54152edf7a39c7b9c7835faaa9f441
barisser <barisser@gmail.com>
2014-10-20 15:33:12 -0700

    reducing size of batches

--- assemblycoins/workertasks.py
+++ assemblycoins/workertasks.py
@@ -215,8 +215,8 @@ def tx_queue_batches():
         feestotal=0
         rowlist=[]
 
-        if len(txs)>25:  #limit outputs per TX
-          txs=txs[0:25]
+        if len(txs)>15:  #limit outputs per TX
+          txs=txs[0:15]
 
         for tx in txs:
           color_needed=color_needed+tx[5]
